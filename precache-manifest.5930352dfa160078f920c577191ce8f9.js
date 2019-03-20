5698ca6e95956ae643e570d827b53c593718a337
k-jasinska <jasinskak64@gmail.com>
2019-03-20 23:32:13 +0100

    Updates

---
+++ precache-manifest.5930352dfa160078f920c577191ce8f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e9476c40bbc8ed29b9d083e416ed0945",
+    "url": "/catchFriend-React/static/media/bg.e9476c40.jpg"
+  },
+  {
+    "revision": "db8207458d56a2d7efab",
+    "url": "/catchFriend-React/static/js/runtime~main.8e27a13a.js"
+  },
+  {
+    "revision": "6c238bed3c55d39f81b9",
+    "url": "/catchFriend-React/static/js/main.dc1f764f.chunk.js"
+  },
+  {
+    "revision": "e70a3cab4e4408c60fa4",
+    "url": "/catchFriend-React/static/js/2.10117d87.chunk.js"
+  },
+  {
+    "revision": "6c238bed3c55d39f81b9",
+    "url": "/catchFriend-React/static/css/main.9066ba5f.chunk.css"
+  },
+  {
+    "revision": "ffbf2305bd169242993f4026246c5996",
+    "url": "/catchFriend-React/index.html"
+  }
+];
